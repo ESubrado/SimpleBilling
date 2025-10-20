58e16b9e33753ca9f05ea050aa07c9ec8a9fbf1e
DEVPC\eugsu <eug.subradojr@gmail.com>
2025-10-20 15:41:00 -0400

    temporary disable eslint

--- bill_client/next.config.ts
+++ bill_client/next.config.ts
@@ -3,7 +3,12 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: path.join(__dirname, "../")
+  outputFileTracingRoot: path.join(__dirname, "../"),
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
